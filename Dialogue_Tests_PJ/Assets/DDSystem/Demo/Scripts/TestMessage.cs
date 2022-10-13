12899b341b5b42747e9f9e4b4f7a02e84220989e
Andy <s1810942008@gms.nutc.edu.tw>
2022-10-13 12:00:51 +0800

    kk

--- Dialogue_Tests_PJ/Assets/DDSystem/Demo/Scripts/TestMessage.cs
+++ Dialogue_Tests_PJ/Assets/DDSystem/Demo/Scripts/TestMessage.cs
@@ -16,53 +16,69 @@ public class TestMessage : MonoBehaviour
         var dialogTexts = new List<DialogData>();
 
         dialogTexts.Add(new DialogData(
-            cmdManager.ChangeSize(E_Up_Down_Init.up) +
-            "字體加大" + cmdManager.ChangeSize(E_Up_Down_Init.init) +
-            "預設字體大小", "Li"));
+            "我最近" +
+            // "\n" +
+            "在對別人的專案動手動腳", "Li"));
 
         dialogTexts.Add(new DialogData(
-            "換圖片", "Sa"));
+            "這是我婆，給你看一下", "Sa"));
+        dialogTexts.Add(new DialogData(
+            "其實是想告訴你" +
+            cmdManager.ChangeEmotion("Sad") +
+            "可以換圖片啦", "Sa"));
 
-        dialogTexts.Add(new DialogData("This Asset, The D'Dialog System has many features.", "Li"));
+        dialogTexts.Add(new DialogData("介紹一下功能", "Li"));
 
         dialogTexts.Add(new DialogData(
-            "You can easily change text " +
-            cmdManager.ChangeColor(E_TextColor.red)+
-            "color, " +
+            "可以改變字體" +
+            cmdManager.ChangeColor(E_TextColor.red) +
+            "顏色，" +
             "/color:white/" +
-            "and " +
+            "還有" +"\n"+
             "/size:up//size:up/" +
-            "size" +
-            cmdManager.ChangeSize(E_Up_Down_Init.init)+
-            " like this.",
+            "字體大小" +
+            cmdManager.ChangeSize(E_Up_Down_Init.init) +
+            "看一下差別",
             "Li", () => Show_Example(0)));
-        dialogTexts.Add(new DialogData(
-            "You can easily change text" + cmdManager.ChangeColor(E_TextColor.aqua) +
-            " color, and /size:up//size:up/size/size:init/ like this.", "Li"));
 
-        dialogTexts.Add(new DialogData("You can easily change text", "Li"));
-        dialogTexts.Add(new DialogData("Just put the command in the string!", "Li", () => Show_Example(1)));
+        dialogTexts.Add(new DialogData("我順便做了一些methods", "Li"));
+        dialogTexts.Add(new DialogData("讓使用者不容易打錯，" +
+                                       cmdManager.ChangeEmotion("Sad") +
+                                       "畢竟我討厭字串", "Li", () => Show_Example(1)));
 
         dialogTexts.Add(new DialogData(
-            "You can also change the character's sprite /emote:Sad/like this, /click//emote:Happy/Smile.", "Li",
+            "還可以改表情 /emote:Sad/但其實只是換圖（心虛）" +
+            cmdManager.WaitingClick() +
+            "/emote:Happy/" +
+            "美工若有需求可以再試著修改成用圖層方式改變表情" +
+            "", "Li",
             () => Show_Example(2)));
 
-        dialogTexts.Add(new DialogData("If you need an emphasis effect, /wait:0.5/wait... /click/or click command.",
+        dialogTexts.Add(new DialogData(
+            "這個插件提供一些氣氛營造的效果，像是等個1秒..."
+            + cmdManager.Wait_for_Seconds(1) +
+            "1秒過去了， 或是點擊滑鼠/click/才出現下一句",
             "Li", () => Show_Example(3)));
 
-        dialogTexts.Add(new DialogData("Text can be /speed:down/slow... /speed:init//speed:up/or fast.", "Li",
+        dialogTexts.Add(new DialogData("放慢文字顯示速度 /speed:down/放慢放慢放慢... /speed:init//speed:up/然後突然變快", "Li",
             () => Show_Example(4)));
 
         dialogTexts.Add(new DialogData(
-            "You don't even need to click on the window like this.../speed:0.1/ tada!/close/", "Li",
+            "也可以自動進入下一句" +
+            cmdManager.ChangeSpeed(0.01f) +
+            " 看仔細........................................................................../close/", "Li",
             () => Show_Example(5)));
 
-        dialogTexts.Add(new DialogData("/speed:0.1/AND YOU CAN'T SKIP THIS SENTENCE.", "Li", () => Show_Example(6),
+        dialogTexts.Add(new DialogData("/speed:0.1/這句不給你skip", "Li", () => Show_Example(6),
             false));
 
-        dialogTexts.Add(new DialogData("And here we go, the haha sound! /click//sound:haha/haha.", "Li", null, false));
+        //若只要不能跳過的效果，action那邊給個null即可，這個class只有兩個多載
+        dialogTexts.Add(new DialogData("當然音效必不可少 /click/" +
+                                       cmdManager.PlaySound("haha") +
+                                       "搞啥啊（關西腔）", "Li", null, false));
+        dialogTexts.Add(new DialogData("已經跟之前的音效播放專案做了整合，整理資源時應該會比較方便吧","Sa"));
 
-        dialogTexts.Add(new DialogData("That's it! Please check the documents. Good luck to you.", "Sa"));
+        dialogTexts.Add(new DialogData("之後還有選項的功能，但我還沒看，先這樣", "Sa"));
 
         DialogManager.Show(dialogTexts);
     }
