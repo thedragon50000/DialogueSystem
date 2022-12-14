07ce9f307a2499ad5eb79b64923ce9291c6f1c9e
Andy <s1810942008@gms.nutc.edu.tw>
2022-12-14 21:08:39 +0800

    Delete useless audio

--- Dialogue_Tests_PJ/Assets/andy/Script/DontDestroy_sc.cs
+++ Dialogue_Tests_PJ/Assets/andy/Script/DontDestroy_sc.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 using UnityEngine.Events;
@@ -8,19 +9,38 @@ using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
 using Button = UnityEngine.UI.Button;
 using UniRx;
+using UnityEngine.Audio;
+using DG.Tweening;
 
 public class DontDestroy_sc : MonoBehaviour
 {
     public AudioSource audio;
+    public AudioMixerGroup mixer1;
+    public AudioMixerGroup mixer15;
+    public AudioMixerGroup mixer2;
 
     private void Start()
     {
-        Observable.EveryUpdate().Where(_ => Input.GetKeyUp(KeyCode.Space)).Subscribe(_ =>
-            audio.Play());
+        Observable.EveryUpdate().ThrottleFirst(TimeSpan.FromSeconds(1)).Subscribe(_ => PlaySound());
+
+        IEnumerator i = PlayFourTimes_ChangePitch();
+        var fromCoroutine = Observable.FromCoroutine(PlayFourTimes_ChangePitch);
+        Observable.EveryUpdate().ThrottleFirst(TimeSpan.FromSeconds(12))
+            .Subscribe(_ => { fromCoroutine.StartAsCoroutine(); });
     }
 
-    private void Awake()
+    public IEnumerator PlayFourTimes_ChangePitch()
     {
-        // DontDestroyOnLoad(this);
+        yield return audio.outputAudioMixerGroup = mixer1;
+        yield return new WaitForSeconds(4);
+        yield return audio.outputAudioMixerGroup = mixer15;
+        yield return new WaitForSeconds(4);
+        yield return audio.outputAudioMixerGroup = mixer2;
+        yield return new WaitForSeconds(4);
+    }
+
+    private void PlaySound()
+    {
+        audio.Play();
     }
 }
