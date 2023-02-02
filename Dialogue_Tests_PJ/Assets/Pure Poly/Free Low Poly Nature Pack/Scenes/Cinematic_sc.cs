e9bb30cea0b86e598e19d5d8885bcca0b247deac
Andy <s1810942008@gms.nutc.edu.tw>
2023-02-02 20:02:27 +0800

    Scene Building

--- Dialogue_Tests_PJ/Assets/Pure Poly/Free Low Poly Nature Pack/Scenes/Cinematic_sc.cs
+++ Dialogue_Tests_PJ/Assets/Pure Poly/Free Low Poly Nature Pack/Scenes/Cinematic_sc.cs
@@ -10,9 +10,12 @@ using Sequence = DG.Tweening.Sequence;
 public class Cinematic_sc : MonoBehaviour
 {
     private Sequence _sequence;
+    private float _fDefaultCameraViewField;
+    private Sequence _doSequence;
 
     private void Awake()
     {
+        _fDefaultCameraViewField = Camera.main.fieldOfView;
         Vector3 forward = transform.position + Vector3.back * 40;
 
         _sequence = DOTween.Sequence();
@@ -20,7 +23,18 @@ public class Cinematic_sc : MonoBehaviour
         var doJump = transform.DOJump(forward, 1, 10, 10).Pause();
         _sequence.SetAutoKill(true);
         var doRotate = transform.DORotate(transform.rotation.eulerAngles+Vector3.back, 5);
-        _sequence.Append(doJump).Join(doRotate);
+        _sequence.Append(doJump).Join(doRotate).Pause();
+
+        var tweenerZoom = DOTween.To(() =>
+                Camera.main.orthographicSize,
+            x => Camera.main.orthographicSize = x,
+            4f, 0).Pause();
+
+        var doRotateCamera = Camera.main.transform.DORotate(new Vector3(0, -200, 0), 1).Pause();
+        var doRotate2Right = Camera.main.transform.DORotate(new Vector3(0, -180, 0), 1).Pause();
+
+        _doSequence = DOTween.Sequence();
+        _doSequence.Append(tweenerZoom).Append(doRotateCamera).Append(doRotate2Right).Pause();
     }
 
     void Start()
@@ -29,7 +43,9 @@ public class Cinematic_sc : MonoBehaviour
         {
             if (Input.GetKeyUp(KeyCode.Space))
             {
-                _sequence.PlayForward();
+                // DOTween.To(() => Camera.main.fieldOfView, x => Camera.main.fieldOfView = x, 30, 1);
+                // _sequence.PlayForward();
+                _doSequence.PlayForward();
             }
         });
     }
